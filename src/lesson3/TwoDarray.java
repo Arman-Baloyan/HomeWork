d0699c70105ab7a8f1401f3892e7909fdeb70e6c
Arman-Baloyan <arman.baloyan@mail.ru>
2021-10-05 01:43:09 +0400

    lesson3 example all

---
+++ src/lesson3/TwoDarray.java
@@ -0,0 +1,21 @@
+package lesson3;
+
+public class TwoDarray {
+    public static void main(String  args[]){
+        int twoD[][] = new int[4] [5];
+        int i, j,k = 0;
+        for ( i = 0; i<4;i++) {
+            for(j = 0; j<5; j++){
+                twoD[i] [j] =k;
+                k++;
+            }
+            for(i=0;i<4; i++){
+                for(j=0;j<5;j++)
+                   System.out.println(twoD[i][j]+" ") ;
+                System.out.println();
+            }
+
+            //попытки и учение готовность почти -2 +2 этот эффект называеться  Tozzerov
+        }
+    }
+}
